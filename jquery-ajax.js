381f13f1f5b119b31d3dc93888fdfdebf4340bc8
Rilag001 <rickylagerkvist@hotmail.com>
2016-05-16 13:32:09 +0200

    Update jquery-ajax.js

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -1,3 +1,20 @@
 // SMHI API
 
+
 // OMDb API
+$('#button-new-movie').click(function() {
+  // Gör en request med jQuery mot OMDb's API
+  
+  var movieURL = "http://www.omdbapi.com/?s=Spartacus&y=&plot=full&type=movie&r=json&";
+  var posterURL = "http://img.omdbapi.com/?apikey=[" + key + "]&";
+  var key;
+  
+  
+  var username =  $("#spotifyusername").val();
+  
+  $.get(url+username, function(data, textStatus, jqXHR) {
+    var responseBody = data;
+    
+    $('#spotify-response').text(responseBody.display_name);
+  })
+});
